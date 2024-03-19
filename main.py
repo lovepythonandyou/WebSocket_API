4472ee1d020a34dd2e95b8c2d5aec30fa24dc73d
eremk <eremkinegor2001@gmail.com>
2024-03-19 14:51:35 +0300

    changing the serializer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 import json
+import pandas as pd
+
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 from datetime import datetime
@@ -7,18 +9,62 @@ from dotenv import load_dotenv
 from langchain.chains.conversation.base import ConversationChain
 from langchain.memory import ConversationBufferWindowMemory
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, StringPromptTemplate, PromptTemplate
 from langchain_openai import ChatOpenAI
 from starlette.templating import Jinja2Templates
 
 load_dotenv()
+
 app = FastAPI()
+
+
+class CSVFileHandler:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.file_path, 'a', newline='')
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.file:
+            self.file.close()
+
+
+@app.on_event("startup")
+async def startup_event():
+    global csv_file
+    with CSVFileHandler('chat_log.csv') as file:
+        csv_file = file
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    if csv_file:
+        csv_file.close()
+
+
 templates = Jinja2Templates(directory="templates")
 llm = ChatOpenAI()
 output_parser = StrOutputParser()
 
 
-# Функция для получения истории сообщений
+# Функция для записи в буффер ключ-значений
+def update_memory_from_csv(memory, file_path):
+    # Чтение данных из файла CSV
+    df = pd.read_csv(file_path, encoding='cp1251')
+
+    # Выбор последних трех строк
+    last_three_rows = df.tail(3)
+
+    # Проход по каждой строке и обновление памяти
+    for index, row in last_three_rows.iterrows():
+        input_message = row['сообщение']
+        output_message = row['ответ']
+        memory.save_context({"input": input_message}, {"output": output_message})
+
+
 def get_last_message_history():
     responses = []
 
@@ -38,24 +84,44 @@ def get_last_message_history():
 
 
 res_mes = get_last_message_history()
+# PROMPT = ChatPromptTemplate.from_messages([
+#     ("system", res_mes),
+# ])
 
-daily_context = res_mes
 
+# daily_context = res_mes
+#
+# template = """
+# Here is some context about today: {daily_context}
+# Current conversation:
+# {history}
+# user: {input}
+# AI:"""
+# PROMPT = PromptTemplate.from_template(template).partial(daily_context=daily_context)
+#
+# conversation = ConversationChain(
+#     prompt=PROMPT,
+#     llm=llm,
+#     verbose=True,
+#     memory=ConversationBufferWindowMemory(k=3)
+# )
+
+
+cbwm = ConversationBufferWindowMemory(k=3, return_messages=True)
+update_memory_from_csv(cbwm, 'chat_log.csv')
 template = """
-
-Here is some context about today: {daily_context}
-
 Current conversation:
 {history}
 user: {input}
-AI:"""
-PROMPT = PromptTemplate.from_template(template).partial(daily_context=daily_context)
+AI:
+"""
+PROMPT = PromptTemplate.from_template(template).partial()
 
 conversation = ConversationChain(
     prompt=PROMPT,
     llm=llm,
     verbose=True,
-    memory=ConversationBufferWindowMemory(k=3)
+    memory=cbwm
 )
 
 
@@ -83,22 +149,12 @@ manager = ConnectionManager()
 
 # Функция для записи данных в CSV файл
 def write_to_csv(timestamp, message, result):
-    with open('chat_log.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([timestamp, message, result])
+    writer = csv.writer(csv_file)
+    writer.writerow([timestamp, message, result])
 
 
 # Функция для генерации ответа
 def get_answer(message):
-    # prompt = ChatPromptTemplate.from_messages([
-    #     ("system",
-    #      "Вы самый умный человек"),
-    #     ("user", "{ввод}")
-    # ])
-    #
-    # chain = prompt | llm | output_parser
-
-    # result = chain.invoke({"ввод": message})
     return conversation.predict(input=message)
 
 
@@ -117,6 +173,10 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
             data = await websocket.receive_text()
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             # Выводим в чат сообщение
+
+            res_mes = get_last_message_history()
+            await manager.broadcast(f"INFO: {res_mes}")
+
             await manager.broadcast(f"Client #{client_id} says: {data}")
 
             # Запоминаем ответ, время и переводим их в формат json
